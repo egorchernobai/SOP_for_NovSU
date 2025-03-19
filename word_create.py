5f20cfd2916feb75eab422e191949cec200fb533
Egor Chernobai <Chernobai.E.S@yandex.ru>
2025-03-19 10:24:09 +0300

    Word func
    Добавил последнюю функцию, работу с word фалом

--- word_create.py
+++ word_create.py
@@ -2,41 +2,76 @@ import matplotlib.pyplot as plt
 from collections import Counter
 from docx import Document
 from io import BytesIO
+from parser_csv import parser
+import json
 
-# Ваш массив с данными (например, ответы пользователей)
-data = ['А', 'Б', 'В', 'А', 'А', 'Б', 'А', 'В', 'Г']
 
-# Подсчитываем количество вхождений каждого значения
-counter = Counter(data)
+class Word():
+    def __init__(self, path, path_to_word):
+        self.path = path
+        self.path_to_word = path_to_word
+        with open("settings.json", "r", encoding='utf-8') as setts:
+            self.setts_json = json.load(setts)
+        self.doc = Document()
+        self.csv = parser.parse(path)
 
-# Получаем значения и их процентное соотношение
-total = len(data)
-percentages = [(count / total) * 100 for count in counter.values()]
+        for i in list(self.setts_json['Subjects'].keys()):
+            self.add_subj(i)
+    
+    def add_subj(self, subj):
+        self.doc.add_heading(subj, 1)
+        data1 = self.csv["Предметы"][subj]
+        for i in list(data1.keys()):
+            if len(self.setts_json["Questions_for_subject"][i]) > 1:
+                data = self.csv["Предметы"][subj][i]
+                counter = Counter(data)
+                total = len(data)
+                percentages = [(count / total) * 100 for count in counter.values()]
+                labels = counter.keys()
+                fig, ax = plt.subplots()
+                ax.pie(percentages, labels=labels, autopct='%1.1f%%', startangle=90)
+                ax.axis('equal')
+                img_stream = BytesIO()
+                plt.savefig(img_stream, format='png')
+                img_stream.seek(0)
+                self.doc.add_heading(i, 2)
+                self.doc.add_picture(img_stream)
+                img_stream.close()
+        
+            else:
+                data = self.csv["Предметы"][subj][i]
+                self.doc.add_heading(i, 2)
+                self.doc.add_paragraph("\n".join(map(str, data)))
 
-# Метки для категорий (ответов)
-labels = counter.keys()
+        for i in self.setts_json["Subjects"][subj]:
+            self.add_teacher(i)
+        self.doc.save(self.path_to_word)
+    
 
-# Создаем круговую диаграмму
-fig, ax = plt.subplots()
-ax.pie(percentages, labels=labels, autopct='%1.1f%%', startangle=90)
-ax.axis('equal')  # Равномерное распределение сегментов
+    def add_teacher(self, teach):
+        self.doc.add_heading(teach, 1)
+        data1 = self.csv["Преподаватели"][teach]
+        for i in list(data1.keys()):
+            if len(self.setts_json["Questions_for_teachers"][i]) > 1:
+                data = self.csv["Преподаватели"][teach][i]
+                counter = Counter(data)
+                total = len(data)
+                percentages = [(count / total) * 100 for count in counter.values()]
+                labels = counter.keys()
+                fig, ax = plt.subplots()
+                ax.pie(percentages, labels=labels, autopct='%1.1f%%', startangle=90)
+                ax.axis('equal')
+                img_stream = BytesIO()
+                plt.savefig(img_stream, format='png')
+                img_stream.seek(0)
+                self.doc.add_heading(i, 2)
+                self.doc.add_picture(img_stream)
+                img_stream.close()
+            else:
+                data = self.csv["Преподаватели"][teach][i]
+                self.doc.add_heading(i, 2)
+                self.doc.add_paragraph("\n".join(map(str, data)))
 
-# Сохраняем диаграмму в буфер
-img_stream = BytesIO()
-plt.savefig(img_stream, format='png')
-img_stream.seek(0)
 
-# Создание документа Word
-doc = Document()
-doc.add_heading('Процентное соотношение встречаемости ответов', 0)
+# docs = Word("1.csv", "1.docx")
 
-# Добавляем изображение диаграммы в Word
-doc.add_picture(img_stream)
-
-# Сохраняем документ
-doc.save('chart_in_word.docx')
-
-# Закрываем поток изображения
-img_stream.close()
-
-print("Документ с диаграммой успешно создан!")
